d987d58054521a5eba21b25b35a2bc6c15384801
malolk <malolkkk@yahoo.com>
2016-12-16 15:35:34 +0800

    add test to socket.h and socket.cc

---
+++ src/network/tests/socket_client_test.cc
@@ -0,0 +1,35 @@
+#include "../socket.h"
+#include "../socket_address.h"
+
+#include <string>
+
+#include <unistd.h>
+
+int main() {
+  using namespace glue_network;
+  int fd = Socket::NewSocket();
+  Socket::EnableAddrReuse(fd, 1);
+  Socket::EnablePortReuse(fd, 1);
+  Socket::EnableNonBlock(fd, 0);
+  SocketAddress server_addr("127.0.0.1", 8080);
+  Socket::Connect(fd, server_addr);
+  ::sleep(2);
+  SocketAddress client_addr;
+  Socket::GetSockName(fd, client_addr);
+  LOG_INFO("\nSocket: %s connected to 127.0.0.1:8080\n", client_addr.ToString().c_str());
+
+  ByteBuffer buf;
+  int cnt = 10;
+  while (cnt-- > 0)	{
+	buf.AppendString(client_addr.ToString());
+    LOG_INFO("client send content %s", buf.ToString().c_str());
+	ssize_t sent_num = Socket::Send(fd, buf);
+	LOG_INFO("Client sent %d bytes", sent_num);
+	ssize_t recv_num = Socket::Receive(fd, buf);
+	LOG_INFO("Client recv %d bytes", recv_num);
+	buf.Reset();
+	sleep(1);
+  }
+  Socket::Close(fd);
+  return 0;	
+}
---
+++ src/network/tests/socket_server_test.cc
@@ -0,0 +1,42 @@
+#include "../socket.h"
+#include "../socket_address.h"
+
+#include <string>
+
+int main() {
+  using namespace glue_network;
+  int fd = Socket::NewSocket();
+  Socket::EnableNonBlock(fd, 0);
+  Socket::EnableAddrReuse(fd, 1);
+  Socket::EnablePortReuse(fd, 1);
+  SocketAddress server_addr("127.0.0.1", 8080);
+  Socket::BindAndListen(fd, server_addr, 100);
+
+  LOG_INFO("Listen on %s ...", server_addr.ToString().c_str());
+  while (true) {
+    SocketAddress client_addr;
+    int cli_fd = Socket::Accept(fd, client_addr);
+    if (cli_fd >= 0) {
+      LOG_INFO("Receive Connect from client: %s ", client_addr.ToString().c_str());
+      Socket::EnableNonBlock(cli_fd, 0);
+      Socket::GetPeerName(cli_fd, client_addr);
+      ByteBuffer buf;
+      while (true) {
+        ssize_t read_num = Socket::Receive(cli_fd, buf);  
+        if (read_num == 0) {
+          Socket::Close(cli_fd);
+          LOG_INFO("peer: %s closed", client_addr.ToString().c_str());
+          break;
+        } else if (read_num > 0) {
+          LOG_INFO("Received data from peer: %s ", buf.ToString().c_str());
+          Socket::Send(cli_fd, buf);
+        } else {
+          Socket::Close(cli_fd);
+          LOG_ERROR("Error on receiving from peer : %s on sockfd=%d ", client_addr.ToString().c_str(), cli_fd);
+          break;
+        }
+        buf.Reset();
+      }
+    }
+  }	
+}
