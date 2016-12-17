bfbff8688d8099a01495854aa9257d2e89531279
malolk <malolkkk@yahoo.com>
2016-12-17 21:42:48 +0800

    rewrite epoll.h and epoll.cc

---
+++ src/network/epoll.cc
@@ -0,0 +1,208 @@
+#include "epoll.h"
+#include "event_channel.h"
+
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+#include <pthread.h>
+
+namespace glue_network {
+void Epoll::Initialize() {
+  epoll_fd_ = ::epoll_create1(EPOLL_CLOEXEC);
+  LOG_CHECK(epoll_fd >= 0, "");
+  process_id_ = glue_libbase::ThreadId();
+  wakeup_fd_ = ::eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK);
+  LOG_CHECK(wakeup_fd_ >= 0, "");
+  
+  wakeup_chann.Initialize(std::bind(&Epoll::ReadWakeupChannel, this), 
+                          std::bind(&Epoll::WriteWakeupChannel, this),
+                          std::bind(&Epoll::CloseWakeupChannel, this));
+  wakeup_chann.AddIntoLoopWithRead();
+}
+
+void MustInLoopThread() {
+  LOG_CHECK(process_id_ == glue_libbase::ThreadId(), "Not the thread where epoll created. ");
+}
+
+void Epoll::Stop() {
+  RunLater(std::bind(&Epoll::StopInLoop, this));	
+}
+
+void Epoll::StopInLoop() {
+  MustInLoopThread();
+  running_ = false;      
+  Wakeup();
+}
+
+void Epoll::RunNowOrLater(const CallbackType& req) {   
+  if (process_id_ == glue_libbase::ThreadId()) {
+    req();
+  } else {    
+	RunLater(req);
+  }
+}           
+
+void Epoll::RunLater(const CallbackType& req) {
+  {
+	glue_libbase::MutexLockGuard m(mu_);
+	pending_requests_.push_back(req);    
+  }
+  if (process_id_ != glue_libbase::ThreadId() || 
+      is_handling_events_) {
+    Wakeup();
+  }
+}
+
+void Epoll::AddChannel(EventChannel* ev) {
+  LOG_CHECK(ev != NULL, "");
+  MustInLoopThread();
+#ifndef NDEBUG
+  if (HasChannel(ev)) {
+    LOG_WARN("Already added channel on fd=%d", ev->Fd());
+    return;
+  }
+#endif
+  SetEvents(ev, EPOLL_CTL_ADD);
+  channels_.insert(ev);
+}
+
+void Epoll::UpdateChannel(EventChannel* ev) {
+  LOG_CHECK(ev != NULL, "");
+  MustInLoopThread();
+#ifndef NDEBUG
+  if (!HasChannel(ev)) {
+    LOG_WARN("Channel on fd=%d not added in loop", ev->Fd());
+    return;
+  }
+#endif
+  SetEvents(ev, EPOLL_CTL_MOD);
+}
+
+void Epoll::DelChannel(EventChannel* ev) {
+  LOG_CHECK(ev != NULL, "");
+  MustInLoopThread();
+
+  std::unordered_set<EventChannel*>::iterator it = channels_.find(ev);
+  if (it == channels_.end()) {
+	return;
+  } else {
+	ev->DisableRDWR();
+	SetEvents(ev, EPOLL_CTL_DEL);
+    channels_.erase(it);
+  }
+}
+
+void Epoll::SetEvents(EventChannel* ev, int op) {
+  struct epoll_event event;
+  event.events = ev->Fields();
+  event.data.ptr = ev;
+  int ret = epoll_ctl(epfd, op, ev->Fd(), &event);
+  if (ret) { 
+	LOG_ERROR("epoll_ctl failed on fd=%d", ev->Fd());	
+  }
+}
+
+void Epoll::Run() {
+  MustInLoopThread();
+  LOG_CHECK(!running_, "");
+  running = true;
+  while (running_) {
+	int ret = epoll_wait(epfd, &(*events.begin()), static_cast<int>(events.size()), -1);
+	LOG_CHECK(ret > 0, "");
+	if (static_cast<size_t>(ret) == events.size()) {
+	  events.resize(2 * events.size());
+    }
+	is_handling_events_ = true;
+	HandleEvents(ret);
+	is_handling_events_ = false;
+	HandleRequests();
+  }	
+}
+
+bool Epoll::HasChannel(EventChannel* ev) {
+  std::unordered_set<EventChannel*>::iterator it = channels_.find(ev);
+  return (it != channels_.end());
+}
+
+void Epoll::HandleEvents(int events_num) {
+  MustInLoopThread();
+  for (int index = 0; index < events_num; ++index) {
+	EventChannel* chann_ptr = static_cast<EventChannel*>(events[index].data.ptr);
+	uint32_t ret_fields = events[index].events;
+	HandleEventsImpl(chann_ptr, ret_fields);
+  }
+}
+
+void Epoll::HandleEventsImpl(EventChannel* chann_ptr, uint32_t ret_fields) {
+#ifndef NDEBUG
+  LOG_CHECK(HasChannel(chann_ptr));
+#endif
+  if (ret_fields & EPOLLIN || ret_fields & EPOLLRDHUP || ret_fields & EPOLLPRI) {
+    chann_ptr->HandleRead();
+  } else if (ret_fields & EPOLLOUT) {
+	chann_ptr->HandleWrite();
+  } else if (ret_fields & EPOLLERR) {
+	LOG_WARN("epoll internal error");
+	chann_ptr->HandleClose();
+  } else if (!(ret_fields & EPOLLIN) && (ret_fields & EPOLLHUP)) {
+	LOG_WARN("Hub on fd: %d", chann_ptr->Fd());
+	chann_ptr->HandleClose();
+  }
+}
+
+void Epoll::HandleRequests() {
+  std::vector<CallbackType> req_buf;
+  {
+	MutexLockGuard m(mu);
+	std::swap(req_buf, requests_);
+  }
+  
+  for (std::vector<CallbackType>::iterator it = req_buf.begin();
+	   it != rcvReqs.end(); ++it) {
+    (*it)();
+  }
+}
+
+void Epoll::Wakeup() {
+  uint64_t buf = 1;
+  ssize_t ret = ::write(wakeup_fd_, &buf, sizeof(buf));
+  if (ret < 0) {
+	int err = errno;
+	if (err == EINTR || err == EAGAIN) {
+	  ret = ::write(wakeup_fd_, &buf, sizeof(buf));
+	  LOG_CHECK(ret == sizeof(buf), "");
+	} else {
+	  LOG_ERROR("Wakeup error");		
+	}
+  }
+}
+
+void Epoll::ReadWakeupChannel() {
+  uint64_t buf;
+  ssize_t ret = ::read(wakeup_fd_, &buf, sizeof(buf));
+  if (ret < 0) {
+    int err = errno;
+	if (err == EINTR) {
+	  ret = ::read(wakeup_fd_, &buf, sizeof(buf));
+	  if (ret < 0) {
+	    LOG_WARN("event read error: EINTR");
+      }
+	} else if (err == EAGAIN) {
+	  LOG_WARN("event read: EAGAIN");
+    } else {
+	  LOG_ERROR("Wakeup error");		
+	}
+  }
+}
+
+void Epoll::CloseWakeupChannel() {
+  wakeup_chann_.DisableRDWR();
+}
+
+void Epoll::WriteWakeupChannel() {
+  // Nothing.   
+}
+
+} // namespace glue_network
---
+++ src/network/epoll.h
@@ -0,0 +1,77 @@
+#ifndef GLUE_NETWORK_EPOLL_H_
+#define GLUE_NETWORK_EPOLL_H_
+
+#include "socket.h"
+#include "event_channel.h"
+#include "../libbase/noncopyable.h"
+#include "../libbase/logger.h"
+#include "../libbase/mutexlock.h"
+#include "../libbase/timeutil.h"
+#include "../libbase/thread.h"
+
+#include <vector>
+#include <unordered_set>
+#include <memory>
+#include <functional>
+#include <atomic>
+
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+#include <errno.h>
+
+/* TODO: add timer_queue. */
+namespace glue_network {
+class Epoll: private glue_libbase::Noncopyable {
+ public:
+  typedef std::function<void()> CallbackType;
+  Epoll()
+    : epoll_fd_(-1), wakeup_fd_(-1), wakeup_chann_(this),
+	  process_id_(-1), running_(false), is_handling_events_(false), 
+      events_(default_event_num_) { 
+  }
+
+  ~Epoll() { 	
+	::close(wakeup_fd_);
+    ::close(epoll_fd_);
+  }
+
+  void Initialize();
+  void MustInLoopThread();
+  void Run();
+  void Stop();
+  void RunNowOrLater(const CallbackType& req);
+  void RunLater(const CallbackType& req);
+
+  bool HasChannel(EventChannel* );
+  void AddChannel(EventChannel* );
+  void DelChannel(EventChannel* );
+  void UpdateChannel(EventChannel* );
+	
+ private:
+  void StopInLoop();
+  void SetEvents(EventChannel*, int op);
+  void HandleEvents(int events_num);
+  void HandleEventsImpl(EventChannel*, uint32_t);
+  void HandleRequests();
+  void Wakeup();
+  void CloseWakeupChannel();
+  void ReadWakeupChannel();
+  void WriteWakeupChannel();
+
+  int epoll_fd_;		
+  int wakeup_fd_;
+  EventChannel wakeup_chann_;	
+  pid_t process_id_;
+  std::atomic<bool> running_;
+  std::atomic<bool> is_handling_events_;
+  std::vector<struct epoll_event> events_; 
+  std::vector<CallbackType> pending_requests_; 
+  std::unordered_set<EventChannel*> channels_;
+  glue_libbase::MutexLock mu_;
+
+  static const default_event_num_;
+};	
+} // namespace glue_network
+#endif // GLUE_NETWORK_EPOLL_H_
