28e2c5538cc9ba2b9dac56775d14cf2fe3e05643
malolk <malolkkk@yahoo.com>
2016-12-19 17:07:32 +0800

    rewrite tcp_server.h and tcp_server.cc

---
+++ src/network/tcp_server.cc
@@ -0,0 +1,71 @@
+#include "tcp_server.h"
+
+namespace glue_network {
+
+void TcpServer::ReadListenSocket() {
+  while (true) {
+    int conn_fd = acceptor_.Accept();
+    if (conn_fd >= 0) {
+      dispatch_id_++;
+      Epoll* epoll_ptr = NULL;
+      if (dispatch_id_ == pool_size_) {
+        epoll_ptr = EventLoop::epoll_ptr_;
+        epoll_ptr->RunNowOrLater(std::bind(&TcpServer::NewConnection, this, conn_fd, read_cb_));
+      } else {
+        EventLoop* eventloop_ptr = eventloop_pool_.NextEventLoop();
+        Epoll* epoll_ptr = eventloop_ptr->EpollPtr();
+        epoll_ptr->RunNowOrLater(std::bind(&EventLoop::NewConnection, eventloop_ptr, conn_fd, read_cb_));
+      }
+      LOG_INFO("Accept connection on fd=%d, and transfer to epoll in thread=%d", conn_fd, epoll_ptr->ThreadId());
+    } else {
+      if (conn_fd == Socket::kRETRY) {
+        /* Already accept all the connection. */
+        break;
+      } else {
+        /* Error occured. Maybe there is no enough file descriptors. */
+        LOG_ERROR("error occured when reading listen socket.");
+        break;
+      }
+    }
+  }
+}
+
+void TcpServer::Routine() {
+  Epoll epoller;
+  epoller.Initialize();
+
+  eventloop_pool_.Start();
+  EventChannel acceptor_chann(EventLoop::epoll_ptr_, acceptor_.Fd());
+  /* TODO: Here, WriteListenSocket and CloseListenSocket do nothing but just to be in consistence with 
+   * the EventChannel::Initialize. Fix it later. */
+  acceptor_chann_.Initialize(std::bind(ReadListenSocket, this), WriteListenSocket, CloseListenSocket);
+  acceptor_chann_.AddIntoLoopWithRead();
+  {
+    glue_libbase::MutexLockGuard m(mu_);
+    EventLoop::epoll_ptr_ = &epoller;
+    condvar_.NotifyOne();
+  }
+  epoller.Run();
+  /* Stop the threadpool. */
+  eventloop_pool_.Shutdown();
+  running_ = false;
+}
+
+void TcpServer::StartInThread() {
+  EventLoop::thread_.Start();
+  running_ = true;
+  thread_.Schedule(std::bind(&TcpServer::Routine, this));
+  {
+    glue_libbase::MutexLockGuard m(mu_);
+    while (EventLoop::epoll_ptr_ = NULL) {
+      condvar_.Wait();
+    }
+  }
+}
+
+void TcpServer::Shutdown() {
+  LOG_CHECK(running_, "");
+  EventLoop::Stop();  /* It's thread-safe here. */
+  EventLoop::Join();
+}
+} // namespace glue_network
---
+++ src/network/tcp_server.h
@@ -0,0 +1,69 @@
+#ifndef GLUE_NETWORK_TCPSERVER_H_
+#define GLUE_NETWORK_TCPSERVER_H_
+
+#include "buffer.h"
+#include "socket.h"
+#include "socket_address.h"
+#include "epoll.h"
+#include "connection.h"
+#include "eventloop_pool.h"
+#include "acceptor.h"
+#include "../libbase/mutexlock.h"
+#include "../libbase/condvar.h"
+#include "../libbase/noncopyable.h"
+#include "../libbase/logger.h"
+
+#include <string>
+#include <memory>
+#include <functional>
+#include <map>
+#include <vector>
+#include <atomic>
+
+#include <unistd.h>
+
+namespace glue_network {
+class TcpServer: private glue_libbase::Noncopyable, public EventLoop {
+ public:
+  explicit TcpServer(const SocketAddress& server_addr, 
+                     int pool_size, const Connection::CallbackReadType read_cb) 
+    : dispatch_counter_(0), pool_size_(pool_size), running_(false), mu_(), condvar_(mu_), read_cb_(read_cb), 
+      server_addr_(server_addr), acceptor_(server_addr), eventloop_pool_(pool_size - 1) {
+    LOG_CHECK(pool_size_ > 0, "");
+    if (!read_cb) {
+      LOG_FATAL("Connection read callback should be valide");
+    }
+  }
+	
+  ~TcpServer() {
+  }
+  /* Start in current thread. */
+  void Run() {
+    Routine(); 
+  }
+
+  /* Start in another thread. */
+  void StartInThread();
+  /* Stop the server in another thread. */
+  void Shutdown();
+  
+ private:
+  void Routine();
+  void ReadListenSocket();
+  static void WriteListenSocket() {
+  }
+  static void CloseListenSocket() {
+  }
+  
+  int dispatch_counter_;
+  const int pool_size_;
+  std::atomic<bool> running_;
+  glue_libbase::MutexLock mu_;
+  glue_libbase::CondVar condvar_; /* For synchronously exiting. */
+  Connection::CallbackReadType read_cb_;
+  SocketAddress server_addr_;
+  Acceptor acceptor_;
+  EventLoopPool eventloop_pool_;
+};
+} // namespace glue_network
+#endif // GLUE_NETWORK_TCPSERVER_H_
