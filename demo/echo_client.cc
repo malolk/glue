96c57438be627151d7cc405465edf2c46e17f802
malolk <malolkkk@yahoo.com>
2016-12-22 00:17:26 +0800

    rewrite demo in new code style

---
+++ demo/echo_client.cc
@@ -0,0 +1,39 @@
+#include "network/tcp_client.h"
+
+#include <iostream>
+#include <string>
+
+using namespace glue_network;
+using namespace glue_libbase;
+
+void ReadCallback(std::shared_ptr<SocketConnection::Connection> conn, ByteBuffer& buf) {
+  std::cout << buf.ToString() << std::endl;
+  buf.Reset();
+  std::string data;
+  std::cin >> data;
+  buf.AppendString(data);
+  conn->Send(buf);  
+  std::cout << buf.ReadableBytes() << std::endl;  
+}
+
+void InitCallback(std::shared_ptr<SocketConnection::Connection> conn) {
+  std::string data;
+  std::cin >> data;
+  ByteBuffer tmp;
+  tmp.AppendString(data);
+  conn->Send(tmp);  
+}
+
+int main() {
+  Epoll epoller;
+  epoller.Initialize();
+  
+  SocketAddress server_addr("127.0.0.1", 8080);
+  TcpClient client(server_addr, 10);
+  client.Initialize(InitCallback, ReadCallback);
+  client.Start(&epoller);
+  epoller.Run();
+  return 0;  
+}
+
+
---
+++ demo/echo_server.cc
@@ -0,0 +1,57 @@
+#include "network/tcp_server.h"
+#include "network/socket_address.h"
+#include "network/connection.h"
+#include "network/buffer.h"
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include <unistd.h>  // for getopt
+#include <stdlib.h>  // for atoi
+
+using namespace glue_network;
+
+// your read-callback
+void ReadCallback(std::shared_ptr<Connection>& conn, ByteBuffer& buf) {
+  // echo the data back
+  conn->Send(buf);  
+  std::cout << buf.ReadableBytes() << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  std::string ip_str = "127.0.0.1";
+  uint16_t port = 8080;
+  int thread_num = 4;
+  int opt;
+  while ((opt = getopt(argc, argv, "s:p:t:")) != -1) {
+    switch (opt) {
+      case 's': {
+        ip_str = std::string(optarg);
+        break;
+      }
+      case 'p': {
+        port = static_cast<uint16_t>(atoi(optarg));
+        break;
+      }
+      case 't': {
+        thread_num = atoi(optarg);
+        break;
+      }
+      default:  {
+        std::cerr << "usage: " 
+                     "-s <ip> -p <port> -t <thread num>"
+                  << std::endl;
+        return 1; 
+      }
+    }    
+  }
+  SocketAddress server_addr(ip_str, port);
+  TcpServer srv(server_addr, thread_num, ReadCallback);
+  std::cout << "===========echo server============" << std::endl;
+  srv.Run();
+
+  return 0;
+}
+
+
